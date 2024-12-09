af2de5ab39f5f41d3b796ea6ea464ea0b5089493
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-09 11:29:23 +0600

    added signOut functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,38 @@
 import { Link } from "react-router-dom";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../Firebase/Firebase.config";
+import { useState } from "react";
 
 const Login = () => {
+
+    const [user,setUser]=useState(null)
     
     const auth = getAuth(app)
     const goggleProvider = new GoogleAuthProvider();
 
     const handleGoogleLogin = ()=>{
-        console.log("click lagce re ")
+        signInWithPopup(auth,goggleProvider)
+        .then(result=>{
+            const loggedUser=result.user;
+            console.log(loggedUser)
+            setUser(loggedUser)
+        })
+        .catch(error=>{
+            console.log("error", error.message)
+        })
     }
 
+
+    const handleLogOut = ()=>{
+       
+        signOut(auth)
+        .then(()=>{
+            setUser(null)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
 
 
   return (
@@ -53,6 +75,7 @@ const Login = () => {
                 required
               />
             </div>
+           
 
             <button
               type="submit"
@@ -69,6 +92,13 @@ const Login = () => {
               Sign in with Google
             </button>
           </div>
+          <div className="mt-4">
+            <button 
+            onClick={handleLogOut}
+            className="w-full px-4 py-2 text-sm font-medium text-white bg-red-500 hover:bg-red-600 rounded-lg shadow focus:outline-none focus:ring focus:ring-red-300">
+              Logout
+            </button>
+          </div>
 
           <div className="mt-6 text-center">
             <p className="text-sm text-gray-600">
@@ -79,6 +109,8 @@ const Login = () => {
               first.
             </p>
           </div>
+
+
         </div>
       </div>
     </div>
